7adb168e44749a78a1fda3d87d438767e8d2d9da
Fahim <rynare.of.legend@gmail.com>
2025-07-17 11:13:38 +0700

    777

--- src/routes/api/index.ts
+++ src/routes/api/index.ts
@@ -1,22 +1,19 @@
 import express from 'express'
 var router = express.Router();
 
-import authEndpoint from './auth'
-import supplierEndpoint from './supplier'
-import productCategoryEndpoint from './product_category'
-import trxEndpoint from './trx'
-import dashboardEndpoint from './dashboard'
-import predictionEndpoint from './prediction'
-import productEndpoint from './product'
-import dummyEndpoint from './dummy'
-import envEndpoint from './env'
 import authenticate from '../../middleware/authMiddleware';
+import authRoute from '../../controller/api/authController';
+import dashboardController from '../../controller/api/dashboardController'
+import predictionController from '../../controller/api/predictionController'
+import productCategoryController from '../../controller/api/productCategoriesController';
+import productController from '../../controller/api/productsController';
+import supplierController from '../../controller/api/suppliersController';
+import transactionController from '../../controller/api/transactionController';
+
 
 router.get('/', function (req, res, next) {
     res.send('Read docs.');
 });
-
-router.use('/auth', authEndpoint)
 
 router.use('/is-authenticated', authenticate)
 router.get('/is-authenticated', (req, res) => {
@@ -25,13 +22,52 @@ router.get('/is-authenticated', (req, res) => {
     })
 })
 
-router.use('/', supplierEndpoint)
-router.use('/', productEndpoint)
-router.use('/', productCategoryEndpoint)
-router.use('/', trxEndpoint)
-router.use('/', dashboardEndpoint)
-router.use('/', predictionEndpoint)
-router.use('/', dummyEndpoint)
-router.use('/', envEndpoint)
+
+router.post('/register', authRoute.register);
+router.post('/login', authRoute.login);
+router.post('/forgot-password', authRoute.forgotPasswordSendEmail);
+router.get('/forgot-password/:token', authRoute.forgotPasswordVerifyToken);
+router.patch('/forgot-password/:token', authRoute.forgotPasswordChangePassword);
+router.get('/verify/:token', authRoute.verify);
+router.post('/re-send-email-activation/:token', authRoute.resendEmailVerification);
+router.get('/refresh-token', authRoute.refreshToken);
+router.get('/logout', authenticate, authRoute.logout);
+
+router.get('/dashboard', authenticate, dashboardController.dashboardDataController)
+router.get('/dashboard/trend/:period_type', authenticate, dashboardController.dashboardTrendController)
+router.get('/dashboard/latest/:trx_type/:period_type', authenticate, dashboardController.dashboardLatestPredictionController)
+
+router.post('/prediction-from-file', predictionController.filePrediction)
+router.get('/saved-predictions/purchases/:period_type', authenticate, predictionController.getSavedPurchasePredictions)
+router.get('/saved-predictions/sales/:period_type', authenticate, predictionController.getSavedSalePredictions)
+router.post('/purchase-prediction/:product_id', authenticate, predictionController.purchasePrediction)
+router.post('/sales-prediction/:product_id', authenticate, predictionController.salesPrediction)
+router.post('/smart-prediction/:product_id', authenticate, predictionController.smartPrediction)
+router.post('/detail-prediction', authenticate, predictionController.predictionDetail)
+
+router.post('/product-category', authenticate, productCategoryController.addProductCategory);
+router.post('/product-category/:id', authenticate, productCategoryController.addProductCategory);
+router.get('/product-categories', authenticate, productCategoryController.getProductCategories);
+router.delete('/product-category/:id', authenticate, productCategoryController.deleteProductCategoryRoute);
+
+router.post('/product', authenticate, productController.addProduct);
+router.get('/product/:product_code', authenticate, productController.getProductByProductCodeRoute);
+router.get('/products', authenticate, productController.showAllProducts);
+router.get('/all-products/limited', authenticate, productController.getAllProduct);
+router.get('/product-get-by-id/:id', authenticate, productController.getProductDetail);
+router.patch('/product/:id', authenticate, productController.updateProductRoute);
+router.delete('/product/:id', authenticate, productController.deleteProductRoute);
+router.get('/product-low-stock', authenticate, productController.getLowStockProduct)
+
+router.post('/supplier', authenticate, supplierController.addSupplier);
+router.get('/suppliers', authenticate, supplierController.showAllSupplier);
+router.get('/supplier/:id', authenticate, supplierController.getSupplierDetail);
+router.patch('/supplier/:id', authenticate, supplierController.updateSupplierRoute);
+router.delete('/supplier/:id', authenticate, supplierController.deleteSupplierRoute);
+
+router.post('/restocks', authenticate, transactionController.createRestockRecord);
+router.post('/transactions', authenticate, transactionController.createTransactionRecord);
+router.get('/restocks-history', authenticate, transactionController.showRestockHistoryRoute);
+router.get('/sales-history', authenticate, transactionController.showSalesHistoryRoute);
 
 export default router;
