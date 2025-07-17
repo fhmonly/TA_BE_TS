e51d905f8d4d00c23c2dd4d3661fa1c0e4596546
Fahim <rynare.of.legend@gmail.com>
2025-07-17 07:54:04 +0700

    a

---
+++ src/index.ts
@@ -0,0 +1,14 @@
+import app from './app';
+import http from 'http';
+import os from 'os';
+import { getLocalIP } from './dev-core';
+
+const PORT = parseInt(process.env.PORT || "5000", 10);
+
+const localIP = getLocalIP();
+
+const server = http.createServer(app);
+
+server.listen(PORT, "0.0.0.0", () => {
+    console.log(`🚀 Server running at http://${localIP}:${PORT}`);
+});
